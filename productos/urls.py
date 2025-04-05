41a90606453dc9f6985b5c8c4437c16dd2023c55
zabroso <pmarambi@inf.utfsm.cl>
2025-04-05 00:05:27 -0300

    feat: agregar logica de categorias

--- productos/urls.py
+++ productos/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_productos, name='lista_productos'),
-    path('nuevo/', views.crear_producto, name='crear_producto'),
-    path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
-    path('eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
-    path('reporte-pdf/', views.reporte_inventario_pdf, name='reporte_pdf')
-] 
+    path('', views.home, name='home'),
+    path('productos/', views.lista_productos, name='lista_productos'),
+    path('productos/crear/', views.crear_producto, name='crear_producto'),
+    path('productos/editar/<int:pk>/', views.editar_producto, name='editar_producto'),
+    path('productos/eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
+    path('categorias/', views.lista_categorias, name='listar_categorias'),
+    path('categorias/crear/', views.crear_categoria, name='crear_categoria'),
+    path('categorias/editar/<int:pk>/', views.editar_categoria, name='editar_categoria'),
+    path('categorias/eliminar/<int:pk>/', views.eliminar_categoria, name='eliminar_categoria'),
+    path('reporte-pdf/', views.reporte_inventario_pdf, name='reporte_pdf'),
+]
